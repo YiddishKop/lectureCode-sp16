e1d04028cd49a18ca91a22b59bf664d42e595a65
yiddishkop <yiddishkop@163.com>
2016-09-30 10:40:04 +0800

    add some comments about building a super-class Object as sub-class field

--- lec9/DIY/VengefulSList.java
+++ lec9/DIY/VengefulSList.java
@@ -2,6 +2,15 @@
 /** SList with additional operation printLostItems() which prints all items
   * that have ever been deleted. */
 public class VengefulSList<Item> extends SList<Item>{
+	/*
+	 * 这里我一开始是定义了两个 private fields 来跟踪‘删除链’：
+	 * 	private Node vengeSentinel;
+	 *  private int vengeSize;
+	 * 但是后来发现，可以直接使用父类对象来统筹这两个fields,实际上
+	 * 这也符合我的本意，因为‘删除链’ 本身就是一个 SList,只是自己
+	 * 不敢用。 这样做之后，代码进一步减少，因为直接可以复用父类中
+	 * 的 method.
+	 */
 	private SList<Item> deletedSList;
 	
 	public VengefulSList(){
